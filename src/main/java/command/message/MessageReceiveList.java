0c22886e218d4dd0a0124fa3c067e447b3a1249c
minTann718 <00.mintann@gmail.com>
2025-10-31 14:32:44 +0900

    .

--- src/main/java/command/message/MessageReceiveList.java
+++ src/main/java/command/message/MessageReceiveList.java
@@ -1,15 +1,36 @@
 package command.message;
 
+<<<<<<< Updated upstream
 import javax.servlet.http.HttpServletRequest;
 
 import common.CommonExecute;
+=======
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import common.CommonExecute;
+import common.CommonUtil;
+import dao.MessageDao;
+import dto.MessageDto;
+>>>>>>> Stashed changes
 
 public class MessageReceiveList implements CommonExecute {
 
 	@Override
 	public void execute(HttpServletRequest request) {
 		
+<<<<<<< Updated upstream
 
+=======
+		MessageDao dao = new MessageDao();
+		
+		String myId = CommonUtil.getSessionInfo(request, "id");
+		
+		ArrayList<MessageDto> dtos = dao.getReceiveRequestList(myId);
+		
+		request.setAttribute("r_dtos", dtos);
+>>>>>>> Stashed changes
 	}
 
 }
--- src/main/java/dto/MessageDto.java
+++ src/main/java/dto/MessageDto.java
@@ -1,18 +1,53 @@
 package dto;
 
 public class MessageDto {
+<<<<<<< Updated upstream
 	private String senderId, receiverId, greetingMsg, sendDate, prifileImg, nickname, tagline;
 	
 	//receiveRequestList  mypage > request_list
 	public MessageDto(String senderId, String receiverId, String greetingMsg, String sendDate, String prifileImg,
 			String nickname, String tagline) {
+=======
+<<<<<<< Updated upstream
+	private String senderId;
+	private String receiverId;
+	private String greetingMessage;
+	private String sendDate;
+=======
+	private String requestNo, senderId, receiverId, greetingMsg, sendDate, 
+					profileImg, r_nickname, r_tagline,
+					r_gender, r_country, r_age;
+	
+	
+	
+	//receiveRequestList  mypage > request_list
+	public MessageDto(String requestNo, String senderId, String receiverId, String greetingMsg, String sendDate, String profileImg,
+			String r_nickname,String r_gender, String r_country, String r_age) {
+		this.requestNo = requestNo;
+>>>>>>> Stashed changes
 		this.senderId = senderId;
 		this.receiverId = receiverId;
 		this.greetingMsg = greetingMsg;
 		this.sendDate = sendDate;
+<<<<<<< Updated upstream
 		this.prifileImg = prifileImg;
 		this.nickname = nickname;
 		this.tagline = tagline;
+=======
+		this.profileImg = profileImg;
+		this.r_nickname = r_nickname;
+		this.r_gender = r_gender;
+		this.r_country = r_country;
+		this.r_age = r_age;
+	}
+
+	public String getRequestNo() {
+		return requestNo;
+	}
+
+	public void setRequestNo(String requestNo) {
+		this.requestNo = requestNo;
+>>>>>>> Stashed changes
 	}
 
 	public String getSenderId() {
@@ -47,6 +82,7 @@ public class MessageDto {
 		this.sendDate = sendDate;
 	}
 
+<<<<<<< Updated upstream
 	public String getPrifileImg() {
 		return prifileImg;
 	}
@@ -69,10 +105,62 @@ public class MessageDto {
 
 	public void setTagline(String tagline) {
 		this.tagline = tagline;
+=======
+	public String getProfileImg() {
+		return profileImg;
+	}
+
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+
+	public String getR_nickname() {
+		return r_nickname;
+	}
+
+	public void setR_nickname(String r_nickname) {
+		this.r_nickname = r_nickname;
+	}
+
+	public String getR_tagline() {
+		return r_tagline;
+	}
+
+	public void setR_tagline(String r_tagline) {
+		this.r_tagline = r_tagline;
+	}
+
+	public String getR_gender() {
+		return r_gender;
+	}
+
+	public void setR_gender(String r_gender) {
+		this.r_gender = r_gender;
+	}
+
+	public String getR_country() {
+		return r_country;
+	}
+
+	public void setR_country(String r_country) {
+		this.r_country = r_country;
+	}
+
+	public String getR_age() {
+		return r_age;
+	}
+
+	public void setR_age(String r_age) {
+		this.r_age = r_age;
+>>>>>>> Stashed changes
 	}
 	
 	
 	
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 	
 	
 }
