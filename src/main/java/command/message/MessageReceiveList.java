63c4f6453bc8bedf232a69740bdfc9f17d177d74
minTann718 <00.mintann@gmail.com>
2025-11-03 10:54:50 +0900

    수정

--- src/main/java/command/message/MessageReceiveList.java
+++ src/main/java/command/message/MessageReceiveList.java
@@ -1,10 +1,4 @@
 package command.message;
-
-<<<<<<< Updated upstream
-import javax.servlet.http.HttpServletRequest;
-
-import common.CommonExecute;
-=======
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,16 +7,12 @@ import common.CommonExecute;
 import common.CommonUtil;
 import dao.MessageDao;
 import dto.MessageDto;
->>>>>>> Stashed changes
 
 public class MessageReceiveList implements CommonExecute {
 
 	@Override
 	public void execute(HttpServletRequest request) {
 		
-<<<<<<< Updated upstream
-
-=======
 		MessageDao dao = new MessageDao();
 		
 		String myId = CommonUtil.getSessionInfo(request, "id");
@@ -30,7 +20,7 @@ public class MessageReceiveList implements CommonExecute {
 		ArrayList<MessageDto> dtos = dao.getReceiveRequestList(myId);
 		
 		request.setAttribute("r_dtos", dtos);
->>>>>>> Stashed changes
+
 	}
 
 }
