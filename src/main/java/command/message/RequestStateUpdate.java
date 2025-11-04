0850e00c8d8e0fba6cfb71c1134b8fb77d80baa2
minTann718 <00.mintann@gmail.com>
2025-11-04 09:23:33 +0900

    save
    a

---
+++ src/main/java/command/message/RequestStateUpdate.java
@@ -0,0 +1,29 @@
+package command.message;
+
+import javax.servlet.http.HttpServletRequest;
+
+import common.CommonExecute;
+import dao.MessageDao;
+
+public class RequestStateUpdate implements CommonExecute {
+
+	@Override
+	public void execute(HttpServletRequest request) {
+		MessageDao dao = new MessageDao();
+		
+		String requestId = request.getParameter("requestId");
+		String state = request.getParameter("state");
+		String locate = request.getParameter("locate");
+		
+		String coment="";
+		if(state.equals("accepted")) coment="요청이 수락되었습니다! 대화를 나눠보세요!";
+		else if(state.equals("rejeacted")) coment="요청이 거절되었습니다.";
+		
+		int result = dao.updateRequestState(requestId, state);
+		
+		String msg = result == 1 ? coment : "updateRequestState 실패";
+		
+		
+	}
+
+}
